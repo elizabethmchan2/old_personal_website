fa2f616af4a82f1c19da5a37386e76f13eb9ee50
elizabethmchan2 <elizabethmchan2@gmail.com>
2018-10-06 14:25:35 -0400

    added two urls and changed route names

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+
+  # before was get "/about/about"
+  # url is now /about rather than /about/about
+  # also this changes the route to just "about" rather than "about_about"
+  get "/about", to: "about#about", as: "about"
+
+  get 'welcome/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
+ 
+
